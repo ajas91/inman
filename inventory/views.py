9cf7e0a1a6685df7e8991363e4ce1bf42cac5f1f
Ali Sultan <alisultan@Alis-MacBook-Pro.local>
2022-03-22 18:06:21 +0400

    add templates for inventory

--- inventory/views.py
+++ inventory/views.py
@@ -5,18 +5,18 @@ from .forms import ItemForm
 # Create your views here.
 def inventory(request):
     context = {}
-    render(request,'base.html',context=context)
+    render(request,'inventory.html',context=context)
 
 
 
 
 def newItem(request):
     context = {}
-    render(request,'base.html',context=context)
+    render(request,'itemDetail.html',context=context)
 
 
 
 
 def updateItem(request,pk):
     context = {}
-    render(request,'base.html',context=context)
+    render(request,'itemDetail.html',context=context)
