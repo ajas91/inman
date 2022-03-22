65ba10023a76e624ca027e92d8c77f322b7445b2
Ali Sultan <alisultan@Alis-MacBook-Pro.local>
2022-03-22 19:27:43 +0400

    fix bug

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('inventory/',inventory, name='inventory'),
     path('inventory/newItem/',newItem, name='new_item'),
-    path('inventory/<str:pk>/',updateItem, name='update_item')
+    path('inventory/<str:pk>/',updateItem, name='update_item'),
     path('inventory/category/<str:pk>/',newCategory, name='new_category')
 
 ]
