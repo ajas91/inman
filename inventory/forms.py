f727f6af4ca1bf1fe2b46cead67fa39d873f5493
Ali Sultan <alisultan@Alis-MacBook-Pro.local>
2022-03-22 18:05:02 +0400

    add form fields

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,3 +1,4 @@
+from tkinter import Widget
 from django.forms import ModelForm, TextInput, DateInput, Select
 from .models import *
 from django.utils.translation import gettext_lazy as _
@@ -6,3 +7,57 @@ class ItemForm(ModelForm):
 	class Meta:
 		model = Item
 		fields = '__all__'
+	
+	def __init__(self,*args,**kwargs):
+		super(ItemForm,self).__init__(*args,**kwargs)
+
+		self.fields['item_name'].widget = TextInput(attrs={
+			'class':'form-control',
+		})
+
+		self.fields['desc'].widget = TextInput(attrs={
+			'class':'form-control',
+		})
+
+		self.fields['ordered_qty'].widget = TextInput(attrs={
+			'class':'form-control',
+		})
+
+		self.fields['remaining_qty'].widget = TextInput(attrs={
+			'class':'form-control',
+		})
+		self.fields['remaining_qty'].disabled = True
+
+		self.fields['item_category'].widget = TextInput(attrs={
+			'class':'select-control',
+		})
+
+		self.fields['purchase_price'].widget = TextInput(attrs={
+			'class':'form-control',
+			'placeholder': 'Price in SAR',
+		})
+
+		self.fields['vat'].widget = TextInput(attrs={
+			'class':'form-control',
+		})
+
+		self.fields['shipping_cost'].widget = TextInput(attrs={
+			'class':'form-control',
+			'placeholder': 'Price in OMR',
+		})
+
+		self.fields['other_cost'].widget = TextInput(attrs={
+			'class':'form-control',
+			'placeholder': 'Price in OMR',
+		})
+		self.fields['other_cost'].required = False
+
+		self.fields['profit_margin'].widget = TextInput(attrs={
+			'class':'form-control',
+			'placeholder': 'Percentage (Number only)',
+		})
+
+		self.fields['selling_price'].widget = TextInput(attrs={
+			'class':'form-control',
+			'placeholder': 'Price in OMR',
+		})
