f586baa7d874f9fcecba3fc4374eacb6d06ece0a
Ali Sultan <alisultan@Alis-MacBook-Pro.local>
2022-03-28 12:37:11 +0400

    add order management functionality

--- orders/forms.py
+++ orders/forms.py
@@ -15,7 +15,7 @@ class OrderForm(ModelForm):
 				'class':'form-control',
 				'id':'order_date',
 		        'type': 'datetime-local',
-				'form':'order'
+				# 'form':'order'
 			})
 		# self.fields['order_date'].widget.attrs['class'] = 'form-control'
 		# self.fields['order_date'].widget.attrs['id'] = 'order_date'
@@ -23,15 +23,15 @@ class OrderForm(ModelForm):
 
 		self.fields['customer'].widget.attrs['class'] = 'form-select'
 		self.fields['customer'].widget.attrs['id'] = 'customer'
-		self.fields['customer'].widget.attrs['form'] = 'order' 
+		# self.fields['customer'].widget.attrs['form'] = 'order' 
 
 		self.fields['status'].widget.attrs['class'] = 'form-select'
 		self.fields['status'].widget.attrs['id'] = 'status'
-		self.fields['status'].widget.attrs['form'] = 'order' 
+		# self.fields['status'].widget.attrs['form'] = 'order' 
 
 		self.fields['total'].widget.attrs['class'] = 'form-control omr'
 		self.fields['total'].widget.attrs['id'] = 'total'
-		self.fields['total'].widget.attrs['form'] = 'order' 
+		# self.fields['total'].widget.attrs['form'] = 'order' 
 
 
 
@@ -47,18 +47,18 @@ class OrderLineForm(ModelForm):
 
 		self.fields['item'].widget.attrs['class'] = 'form-select'
 		self.fields['item'].widget.attrs['id'] = 'item'
-		self.fields['item'].widget.attrs['form'] = 'orderLine' 
+		# self.fields['item'].widget.attrs['form'] = 'orderLine' 
 
 		self.fields['qty'].widget.attrs['class'] = 'form-control'
 		self.fields['qty'].widget.attrs['id'] = 'qty'
-		self.fields['qty'].widget.attrs['form'] = 'orderLine' 
+		# self.fields['qty'].widget.attrs['form'] = 'orderLine' 
 
 
 		self.fields['disc'].widget.attrs['class'] = 'form-control omr'
 		self.fields['disc'].widget.attrs['id'] = 'disc'
-		self.fields['disc'].widget.attrs['form'] = 'orderLine' 
+		# self.fields['disc'].widget.attrs['form'] = 'orderLine' 
 
 
 		self.fields['total_price'].widget.attrs['class'] = 'form-control omr'
 		self.fields['total_price'].widget.attrs['id'] = 'total_price'
-		self.fields['total_price'].widget.attrs['form'] = 'orderLine' 
+		# self.fields['total_price'].widget.attrs['form'] = 'orderLine' 
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,9 @@
+import re
+from sys import prefix
 from django.shortcuts import render, redirect
 from .models import *
 from .forms import OrderForm, OrderLineForm
+from django.forms.formsets import formset_factory
 
 # Create your views here.
 def orders(request):
@@ -16,14 +19,39 @@ def orders(request):
 
 def newOrder(request):
     orderForm = OrderForm()
-    orderLineForm = OrderLineForm()
+    orderLineForm = formset_factory(OrderLineForm)
     itemsList = list(Item.objects.values())
     if request.method == 'POST':
-        orderForm = OrderForm(request.POST)
-        orderLineForm = OrderLineForm(request.POST)
-        if orderForm.is_valid() and orderLineForm.is_valid():
-            orderLineForm.save()
-            orderForm.save()
+        # print(request.POST)
+        # orderForm = OrderForm(request.POST,prefix='initial')
+        orderDate = request.POST['order_date']
+        orderCustomer = request.POST['customer']
+        orderStatus = request.POST['status']
+        orderTotal = request.POST['total']
+        order = Order(order_date = orderDate, 
+                      customer = Customer.objects.get(id=int(orderCustomer)),
+                      status = orderStatus,
+                      total = orderTotal)
+        order.save()
+
+        for i in range(int(request.POST['form-TOTAL_FORMS'][0])):
+            item = request.POST[f'form-{i}-item']
+            qty = request.POST[f'form-{i}-qty']
+            disc = request.POST[f'form-{i}-disc']
+            total_price = request.POST[f'form-{i}-total_price']
+            orderLine = OrderLine(item=Item.objects.get(id=int(item)),
+                                  qty=qty,
+                                  disc=disc,
+                                  total_price=total_price,
+                                  order=order
+                                )
+            orderLine.save()
+
+        print(f'$$$$$$$$$ order ID:{order.id}')
+        print(f'######\n form \n {request.POST}')
+        # if orderForm.is_valid() and orderLineForm.is_valid():
+        #     orderForm.save()
+        #     orderLineForm.save()
         return redirect('orders')
 
     context = { 'orderForm':orderForm,
