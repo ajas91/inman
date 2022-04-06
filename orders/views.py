0168b789ce113a46b50b16c51b8dcac0c49dada9
Ali Sultan <ajasultan@gmail.com>
2022-04-06 14:48:37 +0400

    change orderline to inlineformset

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import *
 from .forms import OrderForm, OrderLineForm
-from django.forms.models import modelformset_factory
+from django.forms.models import modelformset_factory, inlineformset_factory
 from django.forms import formset_factory
 
 # Create your views here.
@@ -18,36 +18,52 @@ def orders(request):
 
 def newOrder(request):
     orderForm = OrderForm()
-    orderLineFormset = formset_factory(OrderLineForm)
+    # formset = formset_factory(OrderLineForm)
+    orderLineFormset = inlineformset_factory(Order,OrderLine,OrderLineForm,can_delete=True,extra=1,max_num=10)
+    formset = orderLineFormset()
     itemsList = list(Item.objects.values())
     if request.method == 'POST':
-        print(request.POST)
-        if orderForm.is_valid() and orderLineFormset.is_valid():
-            orderDate = request.POST['order_date']
-            orderCustomer = request.POST['customer']
-            orderStatus = request.POST['status']
-            orderTotal = request.POST['total']
-            order = Order(order_date = orderDate, 
-                        customer = Customer.objects.get(id=int(orderCustomer)),
-                        status = orderStatus,
-                        total = orderTotal)
-            order.save()
-            for i in range(int(request.POST['form-TOTAL_FORMS'][0])):
-                item = request.POST[f'form-{i}-item']
-                qty = request.POST[f'form-{i}-qty']
-                disc = request.POST[f'form-{i}-disc']
-                total_price = request.POST[f'form-{i}-total_price']
-                orderLine = OrderLine(item=Item.objects.get(id=int(item)),
-                                    qty=qty,
-                                    disc=disc,
-                                    total_price=total_price,
-                                    order=order
-                                    )
-                orderLine.save()
+        # print(request.POST)
+        orderForm = OrderForm(request.POST)
+        if orderForm.is_valid():
+            order = orderForm.save(commit=False)
+            formset = orderLineFormset(request.POST,instance=order)
+            print(formset)
+            if formset.is_valid():
+                order.save()
+                formset.save()
+
+    #     orderLineFormset = modelformset_factory(OrderLine, form=OrderLineForm)
+    #     formset = orderLineFormset(queryset=request.POST)
+        # print(f'{orderForm.is_valid()} and {formset.is_valid()}')
+        # if orderForm.is_valid() and formset.is_valid():
+            
+            # orderDate = request.POST['order_date']
+            # orderCustomer = request.POST['customer']
+            # orderStatus = request.POST['status']
+            # orderTotal = request.POST['total']
+            # order = Order(order_date = orderDate, 
+            #             customer = Customer.objects.get(id=int(orderCustomer)),
+            #             status = orderStatus,
+            #             total = orderTotal)
+            # order = orderForm.save()
+            # for i in formset:#range(int(request.POST['form-TOTAL_FORMS'][0])):
+            #     item = request.POST[f'form-{i}-item']
+            #     qty = request.POST[f'form-{i}-qty']
+            #     disc = request.POST[f'form-{i}-disc']
+            #     total_price = request.POST[f'form-{i}-total_price']
+            #     orderLine = OrderLine(item=Item.objects.get(id=int(item)),
+            #                         qty=qty,
+            #                         disc=disc,
+            #                         total_price=total_price,
+            #                         order=order
+            #                         )
+            #     orderLine.save()
         return redirect('orders')
 
     context = { 'orderForm':orderForm,
-                'orderLineForm':orderLineFormset,
+                # 'orderLineForm':orderLineFormset,
+                'orderLineForm':formset,
                 'status':'new',
                 'itemsList':itemsList,
               }
